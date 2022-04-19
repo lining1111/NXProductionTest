b9eb6a62730aa03e604a44a306b7c4b5d550ec09
lining1111 <770677655@qq.com>
2022-04-19 09:51:56 +0800

    更改版本号显示方式

--- main.go
+++ main.go
@@ -12,23 +12,20 @@ import (
 )
 
 func main() {
-
+	var showVersion bool
 	var version = "1.0.0"
 	var port int
 	var broadcastPort int
 
 	//读取传入的参数
+	flag.BoolVar(&showVersion, "v", false, "显示版本号")
 	flag.IntVar(&port, "port", 8080, "http 端口号 默认 8080")
 	flag.IntVar(&broadcastPort, "broadcastPort", 3000, "udp广播端口号 默认 3000")
-
-	if len(os.Args) == 2 {
-		if os.Args[1] == "-v" {
-			fmt.Println("version:", version)
-			os.Exit(1)
-		}
-
-	}
 	flag.Parse()
+	if showVersion {
+		fmt.Println("version:", version)
+		os.Exit(0)
+	}
 
 	var wg sync.WaitGroup
 
